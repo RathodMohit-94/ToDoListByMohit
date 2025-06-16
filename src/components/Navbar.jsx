37a6baace4122be485b3d3b665059af1f943ad2e
RathodMohit-94 <mohitrathod9328@gmail.com>
2025-06-16 18:52:18 +0530

    Created a Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/1.png'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between  bg-gradient-to-r to-blue-800 from-blue-400 '>
+    <div className='flex justify-between border-solid border-white bg-gradient-to-r to-blue-800 from-blue-400 '>
       {/* Logo */}
         <div className='w-12 bg-transparent  justify-center items-center m-2  '>
             <a href="/">
@@ -12,8 +12,8 @@ const Navbar = () => {
         </div>
     {/* Side Bar */}
         <div className=' font-edu-nsw-act-hand flex gap-8 m-2 p-2 flex-row text-wrap text-center text-xl font-bold text-white '>
-            <a className='hover:bg-slate-200 hover:shadow-lg hover:text-black hover:rounded-md h-10 w-16' href="/">Home</a>
-            <a className='hover:bg-slate-200 hover:text-black hover:rounded-md h-10 w-16' href="/">Login</a>
+            <a className='hover:bg-slate-200 pt-1 duration-500 hover:shadow-lg transition-all hover:text-black hover:rounded-md h-10 w-16' href="/">Home</a>
+            <a className='hover:bg-slate-200 pt-1 duration-500 hover:shadow-lg transition-all hover:text-black hover:rounded-md h-10 w-16' href="/">Login</a>
         </div>
     </div>
   )
