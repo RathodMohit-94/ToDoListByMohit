56da063881b9b4836d3b73e59425d4cc45ad1792
RathodMohit-94 <mohitrathod9328@gmail.com>
2025-06-16 18:53:00 +0530

    Added the both of them

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-
+import MainCon from './components/MainCon'
 
 function App() {
 
   return (
     <>
     <Navbar />
+    <MainCon />
     </>
   )
 }
